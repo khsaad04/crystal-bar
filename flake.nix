53b0eca50283c4120f9fed1c8eef004a7ebd6a74
khsaad04 <khsaad0404@gmail.com>
2024-02-10 12:50:49 +0600

    trying to fix

--- flake.nix
+++ flake.nix
@@ -5,18 +5,18 @@
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
   };
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          gtk4
-          gtk4-layer-shell
-          pkg-config
-        ];
-      };
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        gtk4
+        gtk4-layer-shell
+      ];
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
     };
+  };
 }
