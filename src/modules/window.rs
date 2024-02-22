560ef81fe964313d7a80d689f8c96bdb81c3de0c
khsaad04 <khsaad0404@gmail.com>
2024-02-22 12:45:41 +0600

    fix bug in current window title module

--- src/modules/window.rs
+++ src/modules/window.rs
@@ -16,28 +16,48 @@ impl Module<Label> for WindowModule {
 
     fn into_widget(self) -> Label {
         let window = Label::new(None);
-        let current_active_window = hyprland::data::Client::get_active();
-        window.set_label(&current_active_window.unwrap().unwrap().title);
+        window.set_label(&get_current_active_window().unwrap_or("".to_string()));
 
         {
             let mut listener = EventListener::new();
             let (tx, mut rx) = broadcast::channel(10);
 
             RUNTIME.spawn(async move {
+                let tx_1 = tx.clone();
                 listener.add_active_window_change_handler(move |id| {
-                    let _ = tx.send(id);
+                    let _ = tx_1.send(id.unwrap().window_title);
                 });
+
+                let tx_2 = tx.clone();
+                listener.add_workspace_change_handler(move |id| {
+                    let _ = tx_2.send(format!("c:{}", id));
+                });
+
                 let _ = listener.start_listener();
             });
 
             let window = window.clone();
             glib::spawn_future_local(async move {
                 while let Ok(response) = rx.recv().await {
-                    window.set_label(&response.unwrap().window_title);
+                    if !response.starts_with('c') {
+                        window.set_label(&response);
+                    } else {
+                        window
+                            .clone()
+                            .set_label(&get_current_active_window().unwrap_or("".to_string()));
+                    }
                 }
             });
         }
 
         window
+    }
+}
+
+fn get_current_active_window() -> Option<String> {
+    let client = hyprland::data::Client::get_active().unwrap();
+    match client {
+        Some(w) => Some(w.title),
+        None => None,
     }
 }
